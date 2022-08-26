382cafd62be2fed5e552ee572c0a6e57733d2b24
Sushant Sharma <ssushantlb08@gmail.com>
2022-08-26 12:22:00 +0530

    updates

--- portfolio/src/app/component/routed-component/routed-components/contact-me/contact-me.component.ts
+++ portfolio/src/app/component/routed-component/routed-components/contact-me/contact-me.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+// import {httpClient} from
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
+
 export class ContactMeComponent implements OnInit {
 
-  constructor() { }
+  email: any;
+
+
+  constructor(
+    ) { 
+  }
 
   ngOnInit(): void {
   }
 
-  
+  onSubmit(){
+    // this.httpClient.post('https://contact-form-295b4-default-rtdb.firebaseio.com/',[this.])
+  }
 
 }
