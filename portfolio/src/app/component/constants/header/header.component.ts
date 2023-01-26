47b8620d68dea8b54a4e2265bfe629d22181ce3d
Sushant Sharma <ssushantlb08@gmail.com>
2023-01-26 22:54:18 +0530

    Update header.component.ts

--- portfolio/src/app/component/constants/header/header.component.ts
+++ portfolio/src/app/component/constants/header/header.component.ts
@@ -1,23 +1,28 @@
 import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
-   shownav=true;
+  shownav = true;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
-  navToggle(){
-    if(this.shownav===true){
-      this.shownav=false
-    }else{
-      this.shownav=true
-    }console.log(this.shownav);
-  }
+  
+navToggle(){
+  if (this.shownav === true) {
+    this.shownav = false
+  } else {
+    this.shownav = true
+  } console.log(this.shownav);
+}
 
 }
